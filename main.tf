11c4f6d97090b5bec397f53136a932f74fd470f4
Avi9781 <66740594+Avi9781@users.noreply.github.com>
2021-09-24 11:22:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
-provider "aws"{
- region     =  "ap-south-1"
- access_key = "AKIAX546V3FNWE34VGBV"
- secret_key = "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"
-
-}
-
-resource "aws_instance" "web1" {
-  ami           = "ami-04db49c0fb2215364"
+resource "aws_instance" "linux_instance" {
+  ami           = "ami-0a23ccb2cdd9286bb"
   instance_type = "t2.micro"
+  tags = {
+    name = "Terraform created Ec2"
+  }
+  key_name  = "linuxkey"
 }
